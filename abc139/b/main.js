58b0be6ee47083007a1de6aed068ccc31cca2259
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2024-04-17 08:56:51 +0900

    feat: solved abc139 b

--- abc139/b/main.js
+++ abc139/b/main.js
@@ -1,6 +1,29 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    line = input.split("\n");
+    args = line[0].split(' ');
+    powerStripSocket = Number(args[0]);
+    requiredSocket = Number(args[1]);
+
+    powerStrip = calculatePowerStrip(powerStripSocket, requiredSocket);
+
+    console.log(powerStrip);
+}
+
+function calculatePowerStrip(powerStripSocket, requiredSocket) {
+    socket = 1;
+    powerStrip = 0;
+
+    if (requiredSocket === 1) {
+        return 0;
+    }
+
+    while (requiredSocket > socket) {
+        socket--;
+        powerStrip++;
+        socket += powerStripSocket;
+    }
+
+    return powerStrip;
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
