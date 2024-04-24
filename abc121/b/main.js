2de0b89bedb69a684f91c2d7a912948022fde105
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2024-04-25 08:30:13 +0900

    feat: solved abc121 b

--- abc121/b/main.js
+++ abc121/b/main.js
@@ -1,6 +1,21 @@
 function Main(input) {
     input = input.split("\n");
-    console.log(input);
+    const [n, m, c] = input.shift().split(' ').map(e => +e);
+    const b = input.shift().split(' ').map(e => +e);
+    const a = input.map(e => e.split(' ').map(e => +e));
+
+    let correctAnswerCnt = 0;
+    for (let i = 0; i < n; i++) {
+        let sum = c;
+        for (let j = 0; j < m; j++) {
+            sum += a[i][j] * b[j];
+        }
+
+        if (sum > 0) {
+            correctAnswerCnt++;
+        }
+    }
+    console.log(correctAnswerCnt);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
