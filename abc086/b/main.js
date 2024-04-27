609c920f621d9f36dc12d0be11751d7009035f7a
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2024-04-28 08:01:46 +0900

    feat(abc086/b): ac

--- abc086/b/main.js
+++ abc086/b/main.js
@@ -1,6 +1,15 @@
 function Main(input) {
     input = input.split("\n");
-    console.log(input);
+    // 入力
+    const [a, b] = input[0].split(' ').map(Number); 
+    // aをbより一桁上の数字としてbに加算
+    const [_, bDigits] = b.toExponential().split('e')
+    const x = a * Math.pow(10, Number(bDigits) + 1) + b;
+    // 加算した結果の平方数かチェック
+    const root = Math.sqrt(x);
+    const isSquare = Number.isInteger(root);
+    // 結果によってYes/Noを出力
+    console.log(isSquare ? 'Yes' : 'No');
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
