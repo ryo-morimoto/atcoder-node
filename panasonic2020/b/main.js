01f4bfb5ba60198af49c968c97580ede6fa6e0ec
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2024-04-27 10:06:01 +0900

    new: abc157, abc074, abc086

--- panasonic2020/b/main.js
+++ panasonic2020/b/main.js
@@ -1,6 +1,14 @@
 function Main(input) {
     input = input.split("\n");
-    console.log(input);
+    const [h, w] = input.shift().split(' ').map(Number);
+
+    let canReachMathCount = Math.ceil(h * w / 2);
+
+    if (h < 2 || w < 2) {
+        canReachMathCount = 1;   
+    }
+
+    console.log(canReachMathCount);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
