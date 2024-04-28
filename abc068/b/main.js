0e7afb97599702e7a92a00ede2af264b6bd9a78d
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2024-04-29 08:21:35 +0900

    solved(abc068/b): #time 30m AC nが100以下なの見落としていた。。。しっかりと制約は見ましょう！

--- abc068/b/main.js
+++ abc068/b/main.js
@@ -1,6 +1,22 @@
 function Main(input) {
     input = input.split("\n");
-    console.log(input);
+    /**
+     * - 偶数
+     * - 正整数 N
+     * - N以下で2で割り切れる一番大きい数を求める
+     */
+    const n = Number(input.shift());
+
+    // N以下の偶数の配列を生成する
+    const evenNumbers = [];
+    for (let i = 0; 2 ** i <= n; i++) {
+        evenNumbers.push(2 ** i);
+    }
+
+    // 降順で並び替える
+    evenNumbers.sort((a, b) => b - a);
+
+    console.log(evenNumbers[0]);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
