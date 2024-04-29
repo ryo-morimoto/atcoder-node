37d62432560c5c50820857e32f94ff2c85bc5bf1
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2024-04-30 08:45:58 +0900

    solved(abc161/c): 解説を見てAC
    最初は余りを求めるという発想が出来てなくて、途中で気が付いて処理を入れた。
    そのあと、一部の問題がWAになっててよく見てみると、入力した数値が途中で切れていた。
    制約を見てたけど、JavaScriptのNumber型の最大値を認識できてなかったのが問題
    最終的には解説とテストケースを見て改修してACした。

--- abc161/c/main.js
+++ abc161/c/main.js
@@ -1,6 +1,26 @@
 function Main(input) {
     input = input.split("\n");
-    console.log(input);
+    /**
+     * Nの最小値を求める問題
+     * x = ±(N - K)
+     * x = ±(x - K)
+     * N >= K / 2 の場合、Nを小さくできるので計算する。
+     * KがNの公約数の場合、答えは0になりそう。。
+     */
+    // 入力
+    const [n, k] = input.shift().split(' ').map(BigInt);
+    const ans = calMin(n, k);
+    console.log(ans.toString());
+}
+
+function calMin(n, k) {
+    if (n === 0) {
+        return 0;
+    }
+    const x = n % k;
+    const t = k - x
+
+    return x < t ? x : t;
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
