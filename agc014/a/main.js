161bc1346f7f852ef91ef685a7f2f674ba7949d7
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2024-04-30 07:38:52 +0900

    solved(agc014/a): AC! ソースコードキモイから後でキレイにできる方法探す

--- agc014/a/main.js
+++ agc014/a/main.js
@@ -1,6 +1,44 @@
 function Main(input) {
     input = input.split("\n");
-    console.log(input);
+    /**
+     * クッキーA, B, C
+     * 3人は同時に、各々が持っているクッキーを半分ずつに分けて残りの二人にそれぞれを渡す
+     * 誰かのクッキーの個数が奇数個になるまで続ける。
+     * 全員の持ち数がすべて同じ個数の場合は無限に行われるのでエラー
+     */
+
+    // 入力
+    const [a, b, c] = input.shift().split(' ').map(Number);
+
+    const result = cookieGame(a, b, c);
+
+    console.log(result);
+}
+
+function cookieGame(a, b, c) {
+    // すべて同じ個数の場合は、無限に行われる
+    if (a === b && b === c) {
+        if (a % 2 == 1) {
+            return 0
+        } else {
+            return -1;
+        }
+    }
+
+    let count = 0;
+    while (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+        const halfA = Math.floor(a / 2);
+        const halfB = Math.floor(b / 2);
+        const halfC = Math.floor(c / 2);
+        
+        a = halfB + halfC;
+        b = halfA + halfC;
+        c = halfA + halfB;
+
+        count++
+    }
+
+    return count;
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
